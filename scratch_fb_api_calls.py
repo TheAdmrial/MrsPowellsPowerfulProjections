815f73315b5238b716101b6a6f75ccd9d3b4c4aa
TheAdmrial <adamromriell@gmail.com>
2022-04-30 17:27:35 -0600

    practicing api calls with the facebook api py package

--- scratch_fb_api_calls.py
+++ scratch_fb_api_calls.py
@@ -1,7 +1,11 @@
 # loading in libraries
 #%%
+from os import access
 import requests
 from datetime import datetime, timedelta
+import facebook
+import json
+import pandas as pd
 
 #%%
 '''
@@ -13,16 +17,19 @@ I'll check back in a couple of days to see if there has been any movement.
 '''
 
 #%%
+'''
+This code chunk has an api code that has already expired
+'''
 # making a get request
-app_id = '5044226898989028'
-secret_app_id = '4628fe4fbeb89a91f81b622b760d5e49'
-st_token = 'EABHrsph7DZBQBAOXC6TObaAui8xEVcZASpfEUXqeTP4ZA964cvTZCzMjU5vxTAJdTVkGHYFjBfGd8plkXBNjFPgZAeGvvZAv9yZClIAS5fvHdfgx2JZA1lbcMtkTy97FKtt4NEcXt0IRlZCtLIE8ZAWEETfo3FGkG46ZAbgqXQvR6KA467ZAaiAwfvZAXZBIR6edv3OVLk8s8FgUqxSQFYI44C0VH158vXGpJlOMgZD'
+# app_id = '5044226898989028'
+# secret_app_id = '4628fe4fbeb89a91f81b622b760d5e49'
+# st_token = 'EABHrsph7DZBQBAOXC6TObaAui8xEVcZASpfEUXqeTP4ZA964cvTZCzMjU5vxTAJdTVkGHYFjBfGd8plkXBNjFPgZAeGvvZAv9yZClIAS5fvHdfgx2JZA1lbcMtkTy97FKtt4NEcXt0IRlZCtLIE8ZAWEETfo3FGkG46ZAbgqXQvR6KA467ZAaiAwfvZAXZBIR6edv3OVLk8s8FgUqxSQFYI44C0VH158vXGpJlOMgZD'
 
 
 
-response=requests.get('https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=' + app_id + '&client_secret=' + secret_app_id + '&fb_exchange_token=' + st_token)
+# response=requests.get('https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=' + app_id + '&client_secret=' + secret_app_id + '&fb_exchange_token=' + st_token)
 
-print(response.text)
+# print(response.text)
 #%%
 # requesting access to Mrs. Powell's Ammon's page. 
 lt_token = 'EABHrsph7DZBQBAKxgKEgLMws8H1EvexlzomYObz7xWn8sPt8DbxdMyuZAMTgMLBeFkQ6zbYItzFHzmm0FFOIaXsGUi7P0hydufvCa56y4IgxgC18eiGMiY1Qr13VZAmuTbzdZC4shUPeHaeCoAZCcgSj9owCs4kBTWHslMBpk4ooDcZBh2OSWz'
@@ -50,4 +57,18 @@ REXapikey = "EABHrsph7DZBQBAG0RsoAkGEIxbe9ZANs1OkYKY9hD0ZA8GjEudr6508dAXRDJbJHjP
 RIGapikey = "EABHrsph7DZBQBANNuElLPE4rcrWGIxR05ojek1Eh5BnsB7GcgRZCb8ToK5OGsh2U9HQBoChX5l6qF03FMZCMvTlZCgHZB29W2vSoXRBktghRHwGzSskephkhNpJSTTvrQzEZAGQ1xiEsJJEuYPYAsEVMiPqa3BAAy3KrIozxgKG1fSK4TxB5yQAhYWVDPoXDkZD"
 
 #%%
-pages_api_keys=''
+# trying to use the facebook sdk for python package to get post data from Mrs Powells
+graph = facebook.GraphAPI(access_token = IFapikey)
+page_name = "mrspowells"
+fields = ['id','name', 'likes', 'posts']
+fields = ','.join(fields)
+
+page = graph.get_object(mrspowellsIF, fields = fields)
+print(json.dumps(page, indent = 4))
+
+
+# %%
+if_post_dat = pd.DataFrame(page)
+# %%
+if_posts = if_post_dat.posts
+# %%
---
+++ test_new_sum.py
@@ -0,0 +1,15 @@
+def new_sum(iterable):
+    result = 0
+    for val in iterable:
+        result += val
+    return result
+
+def test_new_sum_list():
+    assert new_sum([1,2,3]) == 6
+
+def test_new_sum_tuple():
+    assert new_sum((-1,2,3)) == 6
+
+if __name__ == "__main__":
+    test_new_sum_list()
+    test_new_sum_tuple()
