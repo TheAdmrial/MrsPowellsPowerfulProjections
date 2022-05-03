a4757400f218f1694653ef8c2b1710ba6dab5507
TheAdmrial <adamromriell@gmail.com>
2022-05-02 21:39:03 -0600

    Able to isolate one post id.

--- scratch_fb_api_calls.py
+++ scratch_fb_api_calls.py
@@ -1,6 +1,5 @@
 # loading in libraries
 #%%
-from os import access
 import requests
 from datetime import datetime, timedelta
 import facebook
@@ -34,41 +33,46 @@ This code chunk has an api code that has already expired
 # requesting access to Mrs. Powell's Ammon's page. 
 lt_token = 'EABHrsph7DZBQBAKxgKEgLMws8H1EvexlzomYObz7xWn8sPt8DbxdMyuZAMTgMLBeFkQ6zbYItzFHzmm0FFOIaXsGUi7P0hydufvCa56y4IgxgC18eiGMiY1Qr13VZAmuTbzdZC4shUPeHaeCoAZCcgSj9owCs4kBTWHslMBpk4ooDcZBh2OSWz'
 mrspowellsIF = 'mrspowells'
+mrspowellsREX = 'mrspowellsrexburg'
+mrspowellsRIG = 'mrspowellsrigby'
+IFapikey = "EABHrsph7DZBQBAIGZBwsdIkpp9mcEd7aOtXEnNMd8McoBaALAFcXl89OPaPoSWgZAAZBRs4RORMXOqHzKKw2BxXja70vQWmCrKVoC49b2ujOVsmxC4KxHoPZBiMzJKLa5mZCEut9jhNEyaFa3EK2zjlE3cSyAoLbvH4kWaaZBocG3CNSNEFEwal1dmi1PMrSDEZD"
+REXapikey = "EABHrsph7DZBQBAG0RsoAkGEIxbe9ZANs1OkYKY9hD0ZA8GjEudr6508dAXRDJbJHjPyt9NPjV03CoCnbdVOqG6REctiFhNF185UxVV7GnD8ZAcqzcivaZCjCXbkiIOgiG2lrJ68481oWkyh8ql1G7rUAuaJhXuKpGIZCAx9RNZC8yN34SZCln9iZAC9T58ubD66AZD"
+RIGapikey = "EABHrsph7DZBQBANNuElLPE4rcrWGIxR05ojek1Eh5BnsB7GcgRZCb8ToK5OGsh2U9HQBoChX5l6qF03FMZCMvTlZCgHZB29W2vSoXRBktghRHwGzSskephkhNpJSTTvrQzEZAGQ1xiEsJJEuYPYAsEVMiPqa3BAAy3KrIozxgKG1fSK4TxB5yQAhYWVDPoXDkZD"
 
+#%%
 IFresponce = requests.get('https://graph.facebook.com/' + mrspowellsIF + '?fields=access_token&access_token=' + lt_token)
 
 print(IFresponce.text)
 #%%
-mrspowellsREX = 'mrspowellsrexburg'
+
 
 REXresponce = requests.get('https://graph.facebook.com/' + mrspowellsREX + '?fields=access_token&access_token=' + lt_token)
 
 print(REXresponce.text)
 
 #%%
-mrspowellsRIG = 'mrspowellsrigby'
 
 RIGresponce = requests.get('https://graph.facebook.com/' + mrspowellsRIG + '?fields=access_token&access_token=' + lt_token)
 
 print(RIGresponce.text)
-# %%
-IFapikey = "EABHrsph7DZBQBAIGZBwsdIkpp9mcEd7aOtXEnNMd8McoBaALAFcXl89OPaPoSWgZAAZBRs4RORMXOqHzKKw2BxXja70vQWmCrKVoC49b2ujOVsmxC4KxHoPZBiMzJKLa5mZCEut9jhNEyaFa3EK2zjlE3cSyAoLbvH4kWaaZBocG3CNSNEFEwal1dmi1PMrSDEZD"
-REXapikey = "EABHrsph7DZBQBAG0RsoAkGEIxbe9ZANs1OkYKY9hD0ZA8GjEudr6508dAXRDJbJHjPyt9NPjV03CoCnbdVOqG6REctiFhNF185UxVV7GnD8ZAcqzcivaZCjCXbkiIOgiG2lrJ68481oWkyh8ql1G7rUAuaJhXuKpGIZCAx9RNZC8yN34SZCln9iZAC9T58ubD66AZD"
-RIGapikey = "EABHrsph7DZBQBANNuElLPE4rcrWGIxR05ojek1Eh5BnsB7GcgRZCb8ToK5OGsh2U9HQBoChX5l6qF03FMZCMvTlZCgHZB29W2vSoXRBktghRHwGzSskephkhNpJSTTvrQzEZAGQ1xiEsJJEuYPYAsEVMiPqa3BAAy3KrIozxgKG1fSK4TxB5yQAhYWVDPoXDkZD"
-
 #%%
 # trying to use the facebook sdk for python package to get post data from Mrs Powells
 graph = facebook.GraphAPI(access_token = IFapikey)
-page_name = "mrspowells"
-fields = ['id','name', 'likes', 'posts']
+fields = ['id','name', 'followers_count', 'posts']
 fields = ','.join(fields)
 
 page = graph.get_object(mrspowellsIF, fields = fields)
 print(json.dumps(page, indent = 4))
+#%%
 
-
-# %%
-if_post_dat = pd.DataFrame(page)
-# %%
-if_posts = if_post_dat.posts
-# %%
+page['posts']['data'][0]['id']
+#%%
+if_post_dat = pd.read_json(pd.json_normalize(page))
+if_dat = pd.json_normalize(if_post_dat.data)
+if_dat
+#%%
+'''
+I am able to get all the posts and post id's for the last year for the Mrs.
+Powell's. Next I need to make a data set of that info and loop through the id's 
+in the facebook api to grab the likes, comments and shares.  
+'''
