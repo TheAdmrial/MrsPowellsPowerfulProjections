94fd9f3c26cf01257af27731264c0d4acd719fbe
TheAdmrial <adamromriell@gmail.com>
2022-06-02 22:53:58 -0600

    checking out the csv data files from fb

--- looking_at_fb_dat.py
+++ looking_at_fb_dat.py
@@ -9,9 +9,17 @@ path =os.getcwd()
 path = path+'/data/fb_data_test.csv'
 
 #%%
-dat = pd.read_csv(path, skiprows=0)
+dat = pd.read_csv(path)
 #%%
-dat.describe()
+# making a data dictionary for the lifetime data columns
+dd_cols = dat.columns
+
+# this grabs the columns and the first row
+dd_defs = dat.iloc[0,8:52]
+
+# trying to make a dictionary for the column definitions 
+# dd_fb_dat = pd.DataFrame([dd_cols,dd_defs])
+# dd_fb_dat = dd_fb_dat.melt(id_vars = [], var_name = "cols", value_name="defs")
 # %%
 dat.info()
 #%%
